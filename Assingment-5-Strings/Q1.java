d2416fb93de83a6ecc74d3648012829bde82a178
gogul-dos <gogulakrishnan8870@gmail.com>
2025-03-26 10:39:51 +0530

    Assingment 6

--- Assingment-5-Strings/Q1.java
+++ Assingment-5-Strings/Q1.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Q1{
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the String: ");
